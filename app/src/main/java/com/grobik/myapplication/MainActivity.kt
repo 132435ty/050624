dad0ba3f66d5ae07fcc01056d5b24fbb643852c9
132435ty <Kz89267908477@gmail.com>
2024-06-05 21:04:13 +0300

    Init commit

---
+++ app/src/main/java/com/grobik/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.grobik.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    Lastinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
